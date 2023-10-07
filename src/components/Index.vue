50d7a7d56f29176722ef8a629c79d51a3cd4def1
huyangfeng <huyangfeng>
2023-10-07 09:07:15 +0800

    '解决了一些已知的问题'

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -23,22 +23,9 @@
 							<el-dropdown trigger="click">
 								<span class="el-dropdown-link">
 									<el-avatar shape="square" :size="50" :src="`/src/assets/dynamic.gif`" />
-									<!-- <el-upload ref="uploadRef" class="avatar-uploader"
-										:action="`http://localhost:9999/upload/uploadImagesToLocal`" :limit="1" :on-exceed="handleExceed"
-										:on-success="handleAvatarSuccess" :before-upload="beforeAvatarUpload" :headers="myHeaders" 
-										:auto-upload="false"
-										list-type="picture-card">
-										<template #trigger>
-											<img class="el-upload-list__item-thumbnail" :src="userInfo.avatar" alt=""
-												width="40" />
-										</template>
-									</el-upload> -->
-									
 								</span>
-								
 								<template #dropdown>
 									<el-dropdown-menu>
-										<el-dropdown-item>修改头像</el-dropdown-item>
 										<el-dropdown-item>修改密码</el-dropdown-item>
 										<el-dropdown-item>退出登录</el-dropdown-item>
 									</el-dropdown-menu>
@@ -46,7 +33,6 @@
 							</el-dropdown>
 						</el-col>
 					</el-row>
-<el-button @click="submitUpload">上传</el-button>
 				</el-header>
 				<el-main>
 					<router-view></router-view>
@@ -79,50 +65,7 @@
 		return rts;
 	})
 	
-/* 	const uploadRef = ref()
-	const userStore =  useUserStore();
-	
-	const myHeaders = ref({
-		authorization:''
-	})
 
-	//文件上传成功时的钩子
-	function handleAvatarSuccess(response, uploadFile, uploadFiles) {
-		//this.applicationInfo.aiHeadPicture = response.data
-		//const userStore =  useUserStore();
-		console.log(response.data)
-		userStore.setAvatar(response.data)
-	}
-	
-	//手动确认上传
-	function submitUpload() {
-		myHeaders.value.authorization = userStore.token
-		console.log(userStore)
-		uploadRef.value.submit()
-		console.log(1)
-	}
-	
-	//处理超出
-	function handleExceed(files, uploadFiles) {
-		uploadRef.value.clearFiles()
-		const file = files[0]
-		file.uid = genFileId()
-		uploadRef.value.handleStart(file)
-	}
-	
-	//上传文件之前的钩子，参数为上传的文件， 若返回false或者返回 Promise 且被 reject，则停止上传。
-	function beforeAvatarUpload(rawFile) {
-		if (rawFile.type == 'image/jpeg' || rawFile.type == 'image/gif') {
-			if (rawFile.size / 1024 / 1024 > 2) {
-				ElMessage.error('上传的文件大小不能超过 2MB!')
-				return false
-			}
-			return true
-		}
-		ElMessage.error('上传的图片格式有误！')
-		return false
-	} */
-	
 	onMounted(() => {
 	})
 	
